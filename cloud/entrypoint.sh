a74a462835b401188d21a6e2a7bade6fa857cf8c
lennartmac <lennartpopma@hotmail.com>
2026-03-05 17:10:31 +1100

    Cloud working base version with actual scripting!

--- cloud/entrypoint.sh
+++ cloud/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+echo "Starting MySQL..."
 mysqld_safe &
 
 echo "Wachten tot MySQL opstart..."
@@ -8,18 +9,22 @@ while ! mysqladmin ping --silent; do
     sleep 1
 done
 
-echo "Database configureren..."
-
-# Database aanmaken
+echo "Creating database and app user..."
 mysql -u root <<EOF
-CREATE DATABASE IF NOT EXISTS mydb;
+CREATE DATABASE IF NOT EXISTS py_diski_webshops;
 CREATE USER IF NOT EXISTS 'appuser'@'localhost' IDENTIFIED BY 'app_password';
-GRANT ALL PRIVILEGES ON mydb.* TO 'appuser'@'localhost';
+GRANT ALL PRIVILEGES ON py_diski_webshops.* TO 'appuser'@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
-# Init SQL uitvoeren
-mysql -u root mydb < init.sql
+echo "Downloading R2 dump..."
+curl -L -o dump.sql https://pub-bf3c129fe9d64e8695d474075e0dfcc6.r2.dev/py_diski_webshops_test_container.sql
 
-echo "Start Python script..."
-python script.py
+echo "Importing dump into MySQL..."
+mysql -u root py_diski_webshops < dump.sql
+
+echo "Running Python scripts..."
+python cool_new_printer_all_offers.py
+
+# Keep container alive (optioneel)
+tail -f /dev/null
